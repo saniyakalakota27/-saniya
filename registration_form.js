495d2115270d2b2bef7f0085cff60170960e4c4d
saniyakalakota27 <saniyakalakota27@gmail.com>
2024-11-25 20:39:23 -0800

    Add files via upload

---
+++ registration_form.js
@@ -0,0 +1,17 @@
+function get_info() {
+	document.write {
+		"ENTERED DETAILS ARE:<br>"+
+		"<br><b>First Name : </b>"+document.getElementById("firstname").value+
+		"<br><b>Last Name : </b>"+document.getElementById("lastname").value+
+		"<br><b>Father's Name : </b>"+document.getElementById("fathername").value+
+		"<br><b>Mother's Name : </b>"+document.getElementById("mothername").value+
+		"<br><b>Email : </b>"+document.getElementById("email").value+
+		"<br><b>Phone Number : </b>"+document.getElementById("phonenumber").value+
+		"<br><b>Date Of Birth : </b>"+document.getElementById("dob").value+
+		"<br><b>Gender : </b>"+document.getElementById("gender").value+
+		"<br><b>Highest Educational Qualification : </b>"+document.getElementById("qualification").value+
+		"<br><b>Qualification Type : </b>"+document.getElementById("qualificationtype").value+
+		"<br><h2>REISTRATION SUCCESSFULL</h2>"
+	
+	};
+}
